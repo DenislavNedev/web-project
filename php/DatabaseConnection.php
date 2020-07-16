8f91101c4c2dabb01a2ddba0a320c0100540887a
root <root@petio_yanakiev@abv.bg>
2020-07-16 23:31:56 +0300

    added gitignore

--- php/DatabaseConnection.php
+++ php/DatabaseConnection.php
@@ -5,7 +5,8 @@ class DatabaseConnection {
 	private $connection;
 
 	public function __construct() {
-		$this->connection = new PDO('mysql:host=localhost;dbname=web-planner', 'root', 'admin1234', [
+		require_once("config.php");
+		$this->connection = new PDO('mysql:host=localhost;dbname=web-planner', $dbUser, $dbPass, [
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
 			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 		]);
---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php 
+
+$dbUser = 'root';
+$dbPass = 'admin1234';
+
+?>
