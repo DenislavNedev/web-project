dc79992740a6c417241bd590c9d8cb99bc40bbb4
evaanikolova <eva.nikolova77@gmail.com>
2020-07-16 23:32:16 +0300

    fixed some nav problems

--- php/DatabaseConnection.php
+++ php/DatabaseConnection.php
@@ -5,7 +5,7 @@ class DatabaseConnection {
 	private $connection;
 
 	public function __construct() {
-		$this->connection = new PDO('mysql:host=localhost;dbname=web-planner', 'root', 'admin1234', [
+		$this->connection = new PDO('mysql:host=localhost;dbname=web-planner', 'root', '', [
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
 			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 		]);
