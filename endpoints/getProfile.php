b5dc3e9641ff5432a06031a176240633d0b55047
root <root@petio_yanakiev@abv.bg>
2020-07-17 22:49:48 +0300

    added Profile class

--- endpoints/getProfile.php
+++ endpoints/getProfile.php
@@ -5,25 +5,10 @@ session_start();
 if (isset($_SESSION['username'])) {
 	
 	$username = $_SESSION['username'];
-	require_once ".././php/DatabaseConnection.php";
 
-	$databaseHandle = new DatabaseConnection();
-	$connection = $databaseHandle->getConnection();
-
-	$selectStatement = $connection->prepare("SELECT username, name, email, role, facultyNumber FROM `Users` WHERE username = :username");
-	$result = $selectStatement->execute([
-		'username' => $username
-	]);
-	$databaseUser = $selectStatement->fetch();
-
-	echo json_encode([
-		'status' => true,
-		'username' => $databaseUser['username'],
-		'name' => $databaseUser['name'],
-		'email' => $databaseUser['email'],
-		'role' => $databaseUser['role'],
-		'facultyNumber' => $databaseUser['facultyNumber']
-	]);
+	require_once "../php/Profile.php";
+	$profile = new Profile($username);
+	echo $profile->fetchUser();
 
 } else {
 	echo json_encode([
---
+++ php/Profile.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Profile {
+
+	private $username;
+
+	public function __construct($username) {
+		$this->username = $username;
+	}
+
+	public function fetchUser() {
+		require_once "DatabaseConnection.php";
+
+		$dbHandle = new DatabaseConnection();
+		$connection = $dbHandle->getConnection();
+
+		$selectStatement = $connection->prepare("SELECT username, name, email, role, facultyNumber FROM `Users` WHERE username = :username");
+		$result = $selectStatement->execute([
+			'username' => $this->username
+		]);
+		$databaseUser = $selectStatement->fetch();
+
+		return json_encode([
+			'status' => true,
+			'username' => $databaseUser['username'],
+			'name' => $databaseUser['name'],
+			'email' => $databaseUser['email'],
+			'role' => $databaseUser['role'],
+			'facultyNumber' => $databaseUser['facultyNumber']
+		]);
+	}
+}
+
+?>
